47763b0e76e1102d4d60322d03940e62d33b42c5
Siwa Khongsuphap <siwa.khongsuphap@gmail.com>
2020-02-05 17:26:35 +0700

    Add delete tag

--- controllers/TagController.js
+++ controllers/TagController.js
@@ -13,3 +13,31 @@ export const getAllTags = async (req, res) => {
     });
   }
 }
+
+export const createTag = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const tag = await model.Tag.create({ name });
+    return res.status(200).json({ tag });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: '',
+    });
+  }
+}
+
+export const deleteTag = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await model.Tag.destroy({
+      where: { id },
+    });
+    return res.status(200).json({ id });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: '',
+    });
+  }
+}
--- routers/tagRouter.js
+++ routers/tagRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getAllTags } from '../controllers/TagController';
+import { getAllTags, createTag, deleteTag } from '../controllers/TagController';
 import { ROUTE } from '../constants';
 
 const tagRouter = express.Router();
 
 tagRouter.get(ROUTE.SUB_URL.INDEX, getAllTags);
+tagRouter.post(ROUTE.SUB_URL.INDEX, createTag);
+tagRouter.delete(ROUTE.SUB_URL.BY_ID, deleteTag);
 
 export default tagRouter;
